d6bbfd2c076de89f4b9bcb40002e10e6bed9bf4e
AreebAmmar <areebammarr@gmail.com>
2025-02-11 22:45:04 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('start').addEventListener('click', function() {
+    window.location.href = document.querySelector('#start a').getAttribute('href');
+  });
+  
+  document.getElementById('watch').addEventListener('click', function() {
+    window.location.href = document.querySelector('#watch a').getAttribute('href');
+  });
+  
