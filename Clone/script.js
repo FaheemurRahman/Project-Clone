1af9fcb1efd90abc226ab6afee89bd6a4856092f
FaheemurRahman <125996186+FaheemurRahman@users.noreply.github.com>
2025-02-11 22:45:44 +0530

    Delete Clone directory

--- Clone/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('start').addEventListener('click', function() {
-    window.location.href = document.querySelector('#start a').getAttribute('href');
-  });
-  
-  document.getElementById('watch').addEventListener('click', function() {
-    window.location.href = document.querySelector('#watch a').getAttribute('href');
-  });
-  
